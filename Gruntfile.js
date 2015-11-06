8498730a5f418fb3ea10893a4cb77fe6f0548ca8
Jared Moore <Jmoore1127@gmail.com>
2015-11-06 00:10:44 -0700

    working build

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,6 +6,11 @@ module.exports = function (grunt) {
     grunt.initConfig({
 
         pkg: grunt.file.readJSON('package.json'),
+        subgrunt: {
+            styles: {
+                'bower_components/airu.web.styles': 'default'
+            }
+        },
         typescript: {
             base: {
                 src: '<%= typescript_src %>',
@@ -16,32 +21,118 @@ module.exports = function (grunt) {
                 }
             }
         },
-        watch: {
-            options: {
-                liveReload: true
+        copy: {
+            main: {
+                nonull: true,
+                src: 'index.html',
+                dest: 'build/index.html'
             },
-            styles: {
-                // Which files to watch (all .modules files)
-                files: ['styles/**/*.less', 'app/assets/**/*.less'],
-                tasks: ['less', 'concat','cssmin'],
-                options: {
-                    nospawn: true
-                }
+            main_styles: {
+                nonull: true,
+                src: 'bower_components/airu.web.styles/styles/main.css',
+                dest: 'build/main_styles.css'
+            },
+            app_styles: {
+                nonull: true,
+                src: 'main.css',
+                dest: 'build/main_app.css'
+            },
+            custom: {
+                nonull: true,
+                src: '<%= copy_src %>',
+                dest: '<%= copy_dst %>',
+                expand: true,
+                flatten: true
             }
+        },
+        concat: {
+            styles: {
+                src: ['build/main_app.css','build/main_styles.css'],
+                dest: 'build/static/css/app.css',
+                nonull:true
+            }
+        },
+        cssmin: {
+            app: {
+                src: 'build/static/css/app.css',
+                dest: 'build/static/css/app.min.css'
+            }
+        },
+        clean: {
+            all: ['build/**/*','build/*'],
+            build: ['build/main_styles.css', 'build/main_app.css', 'build/static/css/app.css']
         }
     });
-
-    grunt.registerTask('build:app', function(){
-
-    });
+    grunt.config.set('typescript_src',['app/**/*.ts']);
 
     grunt.loadNpmTasks('grunt-typescript');
+    grunt.loadNpmTasks('grunt-subgrunt');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
 
-    // Default task(s).
-    grunt.config.set('typescript_src',['app/**/*.ts']);
-    grunt.registerTask('default', ['typescript','build:app']);
+    grunt.registerTask('copy:jquery',function(){
+        var done = this.async();
+        setTimeout(function(){
+            grunt.config.set('copy_src','bower_components/jquery/dist/jquery.min.js');
+            grunt.config.set('copy_dst', 'build/static/lib/js/');
+            grunt.task.run('copy:custom');
+            done();
+        });
+    });
+
+    grunt.registerTask('copy:bootstrap-css', function(){
+        var done = this.async();
+        setTimeout(function(){
+            grunt.config.set('copy_src','bower_components/bootstrap/dist/css/bootstrap.min.css');
+            grunt.config.set('copy_dst', 'build/static/lib/css/');
+            grunt.task.run('copy:custom');
+            done();
+        });
+    });
+
+    grunt.registerTask('copy:bootstrap-js', function(){
+        var done = this.async();
+        setTimeout(function(){
+            grunt.config.set('copy_src','bower_components/bootstrap/dist/js/bootstrap.min.js');
+            grunt.config.set('copy_dst', 'build/static/lib/js/');
+            grunt.task.run('copy:custom');
+            done();
+        });
+    });
+
+    grunt.registerTask('copy:bootstrap', ['copy:bootstrap-css', 'copy:bootstrap-js']);
+
+    grunt.registerTask('copy:angular-js',function(){
+        var done = this.async();
+        setTimeout(function(){
+            grunt.config.set('copy_src','bower_components/angular/angular.min.js');
+            grunt.config.set('copy_dst', 'build/static/lib/js//');
+            grunt.task.run('copy:custom');
+            done();
+        });
+    });
+
+    grunt.registerTask('copy:angular-resource', function(){
+        var done = this.async();
+        setTimeout(function(){
+            grunt.config.set('copy_src','bower_components/angular-resource/angular-resource.min.js');
+            grunt.config.set('copy_dst', 'build/static/lib/js/');
+            grunt.task.run('copy:custom');
+            done();
+        });
+    });
+
+    grunt.registerTask('copy:angular', ['copy:angular-js','copy:angular-resource']);
+    grunt.registerTask('build:app', ['typescript','copy:main']);
+    grunt.registerTask('build:libs', ['copy:jquery', 'copy:bootstrap','copy:angular']);
+    grunt.registerTask('build:styles', ['subgrunt:styles', 'copy:app_styles', 'copy:main_styles', 'concat:styles','cssmin:app']);
+    grunt.registerTask('build:dependencies', ['build:styles']);
+    grunt.registerTask('build:all', ['clean:all', 'build:libs', 'build:dependencies', 'build:app', 'clean:build']); //release build
+    grunt.registerTask('build:all-dirty', ['clean:all', 'build:libs', 'build:dependencies', 'build:app']); //don't clean generated files
+
+    // Default task
+    grunt.registerTask('default', ['build:all']);
 };
