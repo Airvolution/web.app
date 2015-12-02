55ac0db9a61c7f0c4f1853aa9c695f42518249f6
Taylor Wilson <taylorw@eng.utah.edu>
2015-12-02 13:12:12 -0700

    wired map API to frontend!!!

--- app/controllers/MapController/MapController.ts
+++ app/controllers/MapController/MapController.ts
@@ -42,76 +42,21 @@ class MapController {
                 }
             }
         });
-
-        $scope.mapEventDetected = 'No events yet...';
-        $scope.markerEventDetected = 'Give me a click...';
-        $scope.url = 'http://air.eng.utah.edu/api/';
-
         // TODO: It would be nice to make an API class
         console.log('bounds: ' + $scope.bounds.northEast.lat);
-        var url = $scope.url + 'ams/map'
+        var url = 'api/ams/map'
         var obj  = { 'northEast': { 'lat': 89, 'lng': 179 }, 'southWest': { 'lat': -89, 'lng': -179 } };
         var data = JSON.stringify(obj);
         console.log('JSON: ' + data);
         $http.post(url, data, {} ).then(
             function(response) {
                 console.log('Success!');
-                console.log('  data: ' + response.data);
                 console.log('  status: ' + response.status);
-                console.log('  headers: ' + response.headers);
-                console.log('  config: ' + response.config);
                 console.log('======================');
-            },
-            function(response) {
-                console.log('Failure!');
-                console.log('  data: ' + response.data);
-                console.log('  status: ' + response.status);
-                console.log('  headers: ' + response.headers);
-                console.log('  config: ' + response.config);
-                console.log('======================');
-            }
-        );
+                
 
-        // DEPRECATED!!
-        //$http.post(url, data, {} ).
-        //    success(function(data, status, headers, config) {
-        //        console.log('Success!');
-        //        console.log('  data: ' + data);
-        //        console.log('  status: ' + status);
-        //        console.log('  headers: ' + headers);
-        //        console.log('  config: ' + config);
-        //        console.log('======================');
-        //    })
-        //    .error(function(data, status, headers, config) {
-        //        console.log('Failure!');
-        //        console.log('  data: ' + data);
-        //        console.log('  status: ' + status);
-        //        console.log('  headers: ' + headers);
-        //        console.log('  config: ' + config);
-        //        console.log('======================');
-        //    });
-
-
-        var response = JSON.parse(this.getMockResponse(4));
-        this.loadDeviceLocations(response);
-
-        $scope.$on('leafletDirectiveMap.map.moveend', function(event) {
-            // Jared, how do I call the private methods at the bottom of this class?
-            $scope.mapEventDetected = "Map moved to " + $scope.bounds.northEast.lat;
-
-            // TODO: API call to get [deviceIDs and locations]
-            console.log('url: ' + $scope.url);
-
-            var resp = {};
-            resp['Taylor'] = { 'lat': 40.1, 'lng': -111.7 };
-            resp['JaredM'] = { 'lat': 40.2, 'lng': -111.8 };
-            resp['JaredP'] = { 'lat': 40.3, 'lng': -111.9 };
-            resp['ZachLo'] = { 'lat': 40.4, 'lng': -111.6 };
-            resp['YourMom'] = { 'lat': 40.5, 'lng': -111.5 };
-            var resp_str = JSON.stringify(resp);
-
-            // TODO: Parse the returned JSON
-            var data = JSON.parse(resp_str);
+            // TODO: Parse the returned DATA into JSON
+            var data = response.data['ams'];
 
             // Add custom attributes to each Marker
             for (var key in data) {
@@ -126,12 +71,59 @@ class MapController {
 
             // Add markers to map
             $scope.markers = data;
+            },
+            function(response) {
+                console.log('Failure!');
+                console.log('  status: ' + response.status);
+                console.log('======================');
+            }
+        );
+
+        //var response = JSON.parse(this.getMockResponse(4));
+        //this.loadDeviceLocations(response);
+
+        $scope.$on('leafletDirectiveMap.map.moveend', function(event) {
+            // Jared, how do I call the private methods at the bottom of this class?
+            // TODO: API call to get [deviceIDs and locations]
+            var url = 'api/ams/map'
+        var obj  = { 'northEast': { 'lat': 89, 'lng': 179 }, 'southWest': { 'lat': -89, 'lng': -179 } };
+        var data = JSON.stringify(obj);
+        console.log('JSON: ' + data);
+        $http.post(url, data, {} ).then(
+            function(response) {
+                console.log('Success!');
+                console.log('  status: ' + response.status);
+                console.log('======================');
+                
+
+            // TODO: Parse the returned DATA into JSON
+            var data = response.data['ams'];
+
+            // Add custom attributes to each Marker
+            for (var key in data) {
+                //console.log('key: ' + key);
+                if (data.hasOwnProperty(key)) {
+                    data[key]['clickable'] = true;
+                    data[key]['riseOnHover'] = true;
+                    data[key]['draggable'] = true;
+                    data[key]['message'] = "Lat: " + data[key]['lat'] + "</br>Lng: " + data[key]['lng'];
+                }
+            }
+
+            // Add markers to map
+            $scope.markers = data;
+            },
+            function(response) {
+                console.log('Failure!');
+                console.log('  status: ' + response.status);
+                console.log('======================');
+            }
+        );
         });
 
         $scope.$on('leafletDirectiveMarker.map.click', function(event, args){
             // Resource on how to add Marker Events
             // https://github.com/angular-ui/ui-leaflet/blob/master/examples/0513-markers-events-example.html
-            $scope.markerEventDetected = event.name;
             console.log('a marker has been clicked');
 
             var data = { 'deviceID': args.modelName };
@@ -144,66 +136,6 @@ class MapController {
             // TODO: Open Details Panel / Details PLot
         });
 
-
-        // TODO: keep this around for the actual Map
-        //    var heat = L.heatLayer([
-        //        [40.1, -111.71, 1.0],
-        //        [40.1, -111.72, 1.0],
-        //        [40.1, -111.73, 1.0],
-        //        [40.1, -111.74, 1.0],
-        //        [40.1, -111.75, 1.0],
-        //        [40.11, -111.71, 1.0],
-        //        [40.12, -111.72, 1.0],
-        //        [40.13, -111.73, 1.0],
-        //        [40.14, -111.74, 1.0],
-        //        [40.15, -111.75, 1.0],
-        //        [40.16, -111.76, 1.0],
-        //        [40.11, -111.71, 1.0],
-        //        [40.12, -111.72, 1.0],
-        //        [40.13, -111.73, 1.0],
-        //        [40.14, -111.714, 1.0],
-        //        [40.15, -111.725, 1.0],
-        //        [40.16, -111.716, 1.0],
-        //        [40.1, -111.8, 1.0],
-        //        [40.1, -111.82, 1.0],
-        //        [40.1, -111.83, 1.0],
-        //        [40.1, -111.84, 1.0],
-        //        [40.1, -111.85, 1.0],
-        //        [40.11, -111.81, 1.0],
-        //        [40.12, -111.82, 1.0],
-        //        [40.13, -111.83, 1.0],
-        //        [40.14, -111.84, 1.0],
-        //        [40.15, -111.85, 1.0],
-        //        [40.16, -111.86, 1.0],
-        //        [40.11, -111.81, 1.0],
-        //        [40.12, -111.82, 1.0],
-        //        [40.13, -111.83, 1.0],
-        //        [40.14, -111.814, 1.0],
-        //        [40.15, -111.825, 1.0],
-        //        [40.16, -111.816, 1.0],
-        //        [40.2, -111.6, 1.0],
-        //        [40.2, -111.62, 1.0],
-        //        [40.2, -111.63, 1.0],
-        //        [40.2, -111.64, 1.0],
-        //        [40.2, -111.65, 1.0],
-        //        [40.21, -111.61, 1.0],
-        //        [40.22, -111.62, 1.0],
-        //        [40.23, -111.63, 1.0],
-        //        [40.24, -111.64, 1.0],
-        //        [40.25, -111.65, 1.0],
-        //        [40.26, -111.66, 1.0],
-        //        [40.21, -111.61, 1.0],
-        //        [40.22, -111.62, 1.0],
-        //        [40.23, -111.63, 1.0],
-        //        [40.24, -111.614, 1.0],
-        //        [40.25, -111.625, 1.0],
-        //        [40.26, -111.616, 1.0],
-        //        [40.1, -111.7, 0.2]
-        //    ], { radius: 50,
-        //        blur: 15,
-        //        maxZoom: 14}).addTo(map);
-        //
-        //});
     }
 
     private handleMapChange(map) {
