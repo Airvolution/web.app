8997d1c6c0837e15342895c177b488b5a746fa6c
Jared Moore <JMoore@dealersocket.com>
2015-12-01 18:04:58 -0700

    config pages added

--- app/directives/module.ts
+++ app/directives/module.ts
@@ -2,11 +2,9 @@
 import services = require('../services/module');
 import controller = require('../controllers/module');
 import SiteNavDirective = require('./SiteNav/SiteNavDirective');
-import HeatMapDirective = require('./HeatMap/HeatMapDirective');
 
 export = angular.module('directives',
     [   "services",
         "controllers"
     ])
-            .directive(SiteNavDirective.htmlName, SiteNavDirective.create)
-            .directive(HeatMapDirective.htmlName, HeatMapDirective.create);
+            .directive(SiteNavDirective.htmlName, SiteNavDirective.create);
