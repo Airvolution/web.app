d16c0e490c991779bdbf349246f304dc0e2fb8f9
Jared Moore <JMoore@dealersocket.com>
2015-12-04 10:08:04 -0700

    fixed overflow

--- app/controllers/MyStations/MyStationsController.ts
+++ app/controllers/MyStations/MyStationsController.ts
@@ -24,7 +24,37 @@ class MyStationsController {
             type: 'error',
             subtype: 'misconfigured',
             stationId: '12347'
-        }
+        },
+        {
+            type: 'error',
+            subtype: 'offline',
+            stationId: '12345'
+        },
+        {
+            type: 'warning',
+            subtype: 'malfunctioning',
+            stationId: '12346'
+        },
+        {
+            type: 'error',
+            subtype: 'misconfigured',
+            stationId: '12347'
+        },
+        {
+            type: 'error',
+            subtype: 'offline',
+            stationId: '12345'
+        },
+        {
+            type: 'warning',
+            subtype: 'malfunctioning',
+            stationId: '12346'
+        },
+        {
+            type: 'error',
+            subtype: 'misconfigured',
+            stationId: '12347'
+        },
     ];
 
     public stations = [
@@ -75,6 +105,198 @@ class MyStationsController {
             lastUpdated: 'Last updated 1/1/1990 at 9:33pm',
             hasError: true,
             hasWarning: false
-        }
+        },
+        {
+            id: '12345',
+            name: 'Home_Station',
+            purpose: 'blah blah blah',
+            owner: 'Joe User',
+            private: true,
+            indoor: true,
+            location: {
+                name: 'Salt Lake City',
+                lat: 40.1,
+                lng: -111.5
+            },
+            lastUpdated: 'ast updated 1/1/1990 at 9:33pm',
+            hasError: true,
+            hasWarning: false
+        },
+        {
+            id: '12346',
+            name: '',
+            purpose: 'blah blah blah',
+            owner: 'Joe User',
+            private: true,
+            indoor: true,
+            location: {
+                name: 'Salt Lake City',
+                lat: 40,
+                lng: -111.6
+            },
+            lastUpdated: 'Last updated 1/1/1990 at 9:33pm',
+            hasError: false,
+            hasWarning: true
+        },
+        {
+            id: '12347',
+            name: 'Home_Station',
+            purpose: 'blah blah blah',
+            owner: 'Joe User',
+            private: true,
+            indoor: true,
+            location: {
+                name: 'Salt Lake City',
+                lat: 39.9,
+                lng: -111.7
+            },
+            lastUpdated: 'Last updated 1/1/1990 at 9:33pm',
+            hasError: true,
+            hasWarning: false
+        },
+        {
+            id: '12345',
+            name: 'Home_Station',
+            purpose: 'blah blah blah',
+            owner: 'Joe User',
+            private: true,
+            indoor: true,
+            location: {
+                name: 'Salt Lake City',
+                lat: 40.1,
+                lng: -111.5
+            },
+            lastUpdated: 'ast updated 1/1/1990 at 9:33pm',
+            hasError: true,
+            hasWarning: false
+        },
+        {
+            id: '12346',
+            name: '',
+            purpose: 'blah blah blah',
+            owner: 'Joe User',
+            private: true,
+            indoor: true,
+            location: {
+                name: 'Salt Lake City',
+                lat: 40,
+                lng: -111.6
+            },
+            lastUpdated: 'Last updated 1/1/1990 at 9:33pm',
+            hasError: false,
+            hasWarning: true
+        },
+        {
+            id: '12347',
+            name: 'Home_Station',
+            purpose: 'blah blah blah',
+            owner: 'Joe User',
+            private: true,
+            indoor: true,
+            location: {
+                name: 'Salt Lake City',
+                lat: 39.9,
+                lng: -111.7
+            },
+            lastUpdated: 'Last updated 1/1/1990 at 9:33pm',
+            hasError: true,
+            hasWarning: false
+        },
+        {
+            id: '12345',
+            name: 'Home_Station',
+            purpose: 'blah blah blah',
+            owner: 'Joe User',
+            private: true,
+            indoor: true,
+            location: {
+                name: 'Salt Lake City',
+                lat: 40.1,
+                lng: -111.5
+            },
+            lastUpdated: 'ast updated 1/1/1990 at 9:33pm',
+            hasError: true,
+            hasWarning: false
+        },
+        {
+            id: '12346',
+            name: '',
+            purpose: 'blah blah blah',
+            owner: 'Joe User',
+            private: true,
+            indoor: true,
+            location: {
+                name: 'Salt Lake City',
+                lat: 40,
+                lng: -111.6
+            },
+            lastUpdated: 'Last updated 1/1/1990 at 9:33pm',
+            hasError: false,
+            hasWarning: true
+        },
+        {
+            id: '12347',
+            name: 'Home_Station',
+            purpose: 'blah blah blah',
+            owner: 'Joe User',
+            private: true,
+            indoor: true,
+            location: {
+                name: 'Salt Lake City',
+                lat: 39.9,
+                lng: -111.7
+            },
+            lastUpdated: 'Last updated 1/1/1990 at 9:33pm',
+            hasError: true,
+            hasWarning: false
+        },
+        {
+            id: '12345',
+            name: 'Home_Station',
+            purpose: 'blah blah blah',
+            owner: 'Joe User',
+            private: true,
+            indoor: true,
+            location: {
+                name: 'Salt Lake City',
+                lat: 40.1,
+                lng: -111.5
+            },
+            lastUpdated: 'ast updated 1/1/1990 at 9:33pm',
+            hasError: true,
+            hasWarning: false
+        },
+        {
+            id: '12346',
+            name: '',
+            purpose: 'blah blah blah',
+            owner: 'Joe User',
+            private: true,
+            indoor: true,
+            location: {
+                name: 'Salt Lake City',
+                lat: 40,
+                lng: -111.6
+            },
+            lastUpdated: 'Last updated 1/1/1990 at 9:33pm',
+            hasError: false,
+            hasWarning: true
+        },
+        {
+            id: '12347',
+            name: 'Home_Station',
+            purpose: 'blah blah blah',
+            owner: 'Joe User',
+            private: true,
+            indoor: true,
+            location: {
+                name: 'Salt Lake City',
+                lat: 39.9,
+                lng: -111.7
+            },
+            lastUpdated: 'Last updated 1/1/1990 at 9:33pm',
+            hasError: true,
+            hasWarning: false
+        },
     ];
 }
