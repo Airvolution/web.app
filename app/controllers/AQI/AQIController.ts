e19713663e5d98440aed06d5adf787f60ed6b444
TJHooker33 <rolyat.wilson@gmail.com>
2015-12-06 18:04:24 -0700

    attempting to get live aqi

--- app/controllers/AQI/AQIController.ts
+++ app/controllers/AQI/AQIController.ts
@@ -10,6 +10,28 @@ class AQIController {
                 private $http) {
 
         console.log('AQIController constructor');
-        this.localAQI = 77;
+        this.localAQI = '';
+
+        var url = 'api/frontend/aqi'
+        var self = this;
+        $http({
+            url: url,
+            method: 'GET'
+        }).then(
+            function(response) {
+                console.log('Success!');
+                console.log('  status: ' + response.status);
+                console.log('======================');
+
+                // TODO: Parse the returned DATA into JSON
+                var data = response.data;
+                self.localAQI = data['AQI'];
+            },
+            function(response) {
+                console.log('Failure!');
+                console.log('  status: ' + response.status);
+                console.log('======================');
+            }
+        );
     }
 }
