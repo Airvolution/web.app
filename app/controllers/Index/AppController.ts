74ed27ad268e94cecf2d444c8dbd3dba4d8ad813
Jared Moore <JMoore@dealersocket.com>
2015-12-07 18:50:14 -0700

    Toggle Nav

---
+++ app/controllers/Index/AppController.ts
@@ -0,0 +1,19 @@
+///<referecnce path="../../typings/tsd.d.ts"/>
+
+export = AppController;
+
+class AppController {
+    public static name = "AppController";
+    //public navVisible = false;
+    static $inject = ['$scope'];
+    constructor(private $scope){
+        console.log('set navVisible = false on load');
+        $scope.navVisible = false;
+    }
+
+    public toggleNav(){
+        //this.navVisible = !this.navVisible;
+        console.log('toggly toggly toggly');
+        this.$scope.navVisible = !this.$scope.navVisible;
+    }
+}
