c14c45b7099e5b68efc0082f15bab2fe13b7003f
Jared Moore <JMoore@dealersocket.com>
2015-12-07 19:16:45 -0700

    fixed register form

--- app/controllers/RegisterAMS/RegisterAMSController.ts
+++ app/controllers/RegisterAMS/RegisterAMSController.ts
@@ -6,11 +6,17 @@ export = RegisterAMSController;
 class RegisterAMSController {
 
     public static name = "RegisterAMSController";
-    public formData = {};
+    public formData:any = {};
     static $inject = ['$http'];
     constructor(private $http){}
 
     public formSubmit(){
-        this.$http.post(Globals.api_base+'/frontend/registerDevice', this.formData);
+        if(!this.formData.owner)
+            this.formData.owner = '';
+        if(!this.formData.location)
+            this.formData.location = {lat:'',lng:''};
+        if(this.formData.id == undefined || this.formData.private == undefined || this.formData.indoor == undefined)
+            return;
+        this.$http.post('/frontend/registerDevice', this.formData);
     }
 }
