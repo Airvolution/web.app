0d12e44c11f9e5dee1854d1bff93aac4afe6e40b
Taylor Wilson <rolyat.wilson@gmail.com>
2016-01-22 21:26:31 -0700

    more spaces, more single quotes, more things for lint to find

--- app/controllers/PageControllers/ComparePageController.ts
+++ app/controllers/PageControllers/ComparePageController.ts
@@ -1,58 +1,63 @@
 ///<reference path="../../../typings/tsd.d.ts" />
-declare var _;
-declare var d3;
+declare let _;
+declare let d3;
 
 export = ComparePageController;
 
 class ComparePageController {
     public static name = "ComparePageController";
-    static $inject = ['$scope', '$http'];
+    static $inject = ["$scope", "$http"];
 
     public stations = [];
     public plots = [];
-    
+
     constructor(private $scope,
                 private $http) {
-        var self = this;
+        let self = this;
         $http({
-            url: 'api/frontend/getUserDeviceStates',
-            method: 'GET'
+            url: "api/frontend/getUserDeviceStates",
+            method: "GET"
         }).then(function(data){
             self.stations = data.data;
         });
     };
-    
-    public onListChange(id){
-        console.log("onListChange(%s)",id);
-        var plot = _.find(this.plots,function(p){ return id == p.id});
-        if(!!plot){
+
+    public onListChange(id) {
+        console.log("onListChange(%s)", id);
+        let plot = _.find(this.plots, function(p) {
+            return id === p.id;
+        });
+
+        if (!!plot) {
             plot.visible = !plot.visible;
-        }else{
-            plot = {id:id,visible:true,data:[]};
+        } else {
+            plot = {id: id, visible: true, data: []};
             this.generatePlot(plot);
         }
     }
+
     public generatePlot(plot) {
-        console.log('generating plot');
-        var self = this;
-        var data = JSON.stringify(plot.id);
+        console.log("generating plot");
+        let self = this;
+        let data = JSON.stringify(plot.id);
         this.$http({
-            url: 'api/frontend/deviceDatapoints',
+            url: "api/frontend/deviceDatapoints",
             data: data,
-            method: 'POST'
+            method: "POST"
             }).then(
             function(response) {
                 plot.data = response.data;
                 self.plots.push(plot);
             },
             function(response) {
-                console.log('Failure!');
+                console.log("Failure!");
             }
         );
     }
+
     public options = {
         chart: {
-            type: 'stackedAreaChart',
+            type: "stackedAreaChart",
             height: 150,
             margin: {
                 top: 20,
@@ -73,12 +78,12 @@ class ComparePageController {
             xAxis: {
                 showMaxMin: false,
                 tickFormat: function (d) {
-                    return d3.time.format('%x')(new Date(d));
+                    return d3.time.format("%x")(new Date(d));
                 }
             },
             yAxis: {
                 tickFormat: function (d) {
-                    return d3.format(',.2f')(d);
+                    return d3.format(",.2f")(d);
                 }
             },
             zoom: {
@@ -88,7 +93,7 @@ class ComparePageController {
                 useNiceScale: false,
                 horizontalOff: false,
                 verticalOff: true,
-                unzoomEventType: 'dblclick.zoom'
+                unzoomEventType: "dblclick.zoom"
             }
         }
     };
--- app/controllers/PageControllers/MapPageController.ts
+++ app/controllers/PageControllers/MapPageController.ts
@@ -4,20 +4,21 @@ export = MapPageController;
 
 class MapPageController {
     public static name = "MapPageController";
-    static $inject = ['$scope'];
+    static $inject = ["$scope"];
     constructor(
         private $scope
-    ){
+    ) {
         // TODO: at some point I want to fix the showBounds. I'm not sure why it isn't updating anymore. (Scope issue perhaps)
         $scope.showBounds = false;
     }
-    public togglePlot(visibility){        
-        console.log('togglePlot called');
-                
+
+    public togglePlot(visibility) {
+        console.log("togglePlot called");
+
         if (visibility) {
             this.$scope.plotVisible = !this.$scope.plotVisible;
         }
-        
+
         // pctrl is the parent for mctrl and cctrl
         // we want to call a function in cctrl
         if (this.$scope.plotVisible) {
