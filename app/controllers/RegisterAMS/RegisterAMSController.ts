0b72c2507834c178dc8d07cdc656c26f6d33d143
Taylor Wilson <rolyat.wilson@gmail.com>
2016-01-22 21:28:18 -0700

    fixed a few spaces here and a few there

--- app/controllers/RegisterAMS/RegisterAMSController.ts
+++ app/controllers/RegisterAMS/RegisterAMSController.ts
@@ -6,17 +6,17 @@ export = RegisterAMSController;
 class RegisterAMSController {
 
     public static name = "RegisterAMSController";
-    public formData:any = {};
-    static $inject = ['$http'];
-    constructor(private $http){}
+    public formData: any = {};
+    static $inject = ["$http"];
+    constructor(private $http) {}
 
-    public formSubmit(){
-        if(!this.formData.owner)
-            this.formData.owner = '';
-        if(!this.formData.location)
-            this.formData.location = {lat:0,lng:0};
-        if(this.formData.id == undefined || this.formData.private == undefined || this.formData.indoor == undefined)
+    public formSubmit() {
+        if (!this.formData.owner)
+            this.formData.owner = "";
+        if (!this.formData.location)
+            this.formData.location = {lat: 0, lng: 0};
+        if (this.formData.id === undefined || this.formData.private === undefined || this.formData.indoor === undefined)
             return;
-        this.$http.post('/api/frontend/registerUserDevice', this.formData);
+        this.$http.post("/api/frontend/registerUserDevice", this.formData);
     }
 }
