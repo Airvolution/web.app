7fb7a5645b394a9d549219497e5fd97521b07b66
Taylor Wilson <rolyat.wilson@gmail.com>
2016-01-30 15:49:47 -0700

    radar removed from project

--- app/controllers/module.ts
+++ app/controllers/module.ts
@@ -1,6 +1,5 @@
 ///<reference path='../../typings/tsd.d.ts' />
 // import services = require('../services/module');
-import HeatMapController = require('./HeatMap/HeatMapController');
 import MapController = require('./Map/MapController');
 import RegisterAMSController = require('./RegisterAMS/RegisterAMSController');
 import MyProfileController = require('./MyProfile/MyProfileController');
@@ -15,7 +14,6 @@ import AppController = require('./Index/AppController');
 export = angular.module('controllers',
     [   'services'
     ])
-            .controller(HeatMapController.name, HeatMapController)
             .controller(MapController.name, MapController)
             .controller(RegisterAMSController.name, RegisterAMSController)
             .controller(MyProfileController.name, MyProfileController)
