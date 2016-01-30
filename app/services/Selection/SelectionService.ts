ba08e8e29337c3e7736163a3c4ed529992d0da6a
Taylor Wilson <rolyat.wilson@gmail.com>
2016-01-29 23:59:48 -0700

    compare list using selection service to select devices

--- app/services/Selection/SelectionService.ts
+++ app/services/Selection/SelectionService.ts
@@ -12,6 +12,32 @@ class SelectionService {
         // empty constructor
     }
 
+    public addToSelection(stationID) {
+        this.currentSelection.push(stationID);
+    }
+
+    public removeStationFromSelection(stationID) {
+        let index = this.currentSelection.indexOf(stationID);
+        if (index > -1) {
+            this.currentSelection.splice(index, 1);
+        }
+    }
+
+    public removeIndexFromSelection(index) {
+        this.currentSelection.splice(index, 1);
+    }
+
+    public updateSelectionWith(stationID) {
+        let index = this.currentSelection.indexOf(stationID);
+        if (index > -1) {
+            this.removeIndexFromSelection(index);
+            this.$log.log('UPDATE: removing station from list: ' + stationID);
+        } else {
+            this.addToSelection(stationID);
+            this.$log.log('UPDATE: adding station to list: ' + stationID);
+        }
+    }
+
     public setCurrentSelection(stationIDs) {
         this.currentSelection = stationIDs;
     }
