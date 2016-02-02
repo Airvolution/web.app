dfc6eeb5ff8e82d64b9ad28798d9aaa24217c4e7
Jared Moore <JMoore@dealersocket.com>
2016-02-01 23:00:37 -0700

    polished user login

--- typings/tsd.d.ts
+++ typings/tsd.d.ts
@@ -3,3 +3,4 @@
 /// <reference path="jquery/jquery.d.ts" />
 /// <reference path="angular-ui-router/angular-ui-router.d.ts" />
 /// <reference path="leaflet/leaflet.d.ts" />
+/// <reference path="angular-ui-bootstrap/angular-ui-bootstrap.d.ts" />
