15198f221e68c25345cddd690d73a9bf5d252ef1
Taylor Wilson <rolyat.wilson@gmail.com>
2016-02-01 21:50:08 -0700

    fixes register station endpoint

--- app/controllers/RegisterAMS/RegisterAMSController.ts
+++ app/controllers/RegisterAMS/RegisterAMSController.ts
@@ -14,12 +14,15 @@ class RegisterAMSController {
         if (!this.formData.owner) {
             this.formData.owner = '';
         }
+        if (!this.formData.purpose) {
+            this.formData.purpose = '';
+        }
         if (!this.formData.location) {
             this.formData.location = {lat: 0, lng: 0};
         }
         if (this.formData.id == undefined || this.formData.private == undefined || this.formData.indoor == undefined) {
             return;
         }
-        this.$http.post('/api/frontend/registerUserDevice', this.formData);
+        this.$http.post('api/stations/register', this.formData);
     }
 }
