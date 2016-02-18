54335fba8901b1befe3a381323d074c5d4561487
Jared Moore <JMoore@dealersocket.com>
2016-02-18 16:38:01 -0700

    better widgets

--- app/directives/Almanac/AlmanacWidgetController.ts
+++ app/directives/Almanac/AlmanacWidgetController.ts
@@ -3,6 +3,43 @@
 export = AlmanacWidgetController;
 
 class AlmanacWidgetController {
-    public static $inject = [];
-    constructor(){}
+    public type:string;
+    public templateUrl:string;
+    private templateMap = {
+        'avg-aqi-7-days': "avgAQI7Days.html",
+        'consecutive-days': "consecutiveDays.html"
+    };
+
+    private templateSizeMap = {
+        'avg-aqi-7-days': 'medium',
+        'consecutive-days': 'medium'
+    };
+
+    public static $inject = ['$scope'];
+
+    constructor($scope) {
+        var self = this;
+        var deregister = $scope.$watch('ctrl.type', (oldVal, newVal)=> {
+            self.templateUrl = self.getTemplateLocation(newVal);
+            if (newVal != '') {
+                deregister();
+            }
+        });
+    }
+
+    private getTemplateLocation(type) {
+        var prefix = 'app/templates/widgets/';
+        if(this.templateMap[type]){
+            return prefix + this.templateMap[type];
+        } else {
+            return prefix + 'widgetNotFound.html';
+        }
+    }
+    public getTemplateSize(type) {
+        if(this.templateSizeMap[type]){
+            return this.templateSizeMap[type];
+        } else {
+            return 'small';
+        }
+    }
 }
--- app/directives/Almanac/AlmanacWidgetDirective.ts
+++ app/directives/Almanac/AlmanacWidgetDirective.ts
@@ -1,21 +1,43 @@
 /// <reference path="../../../typings/tsd.d.ts" />
 
-import AlmanacViewController = require("./AlmanacViewController");
+import AlmanacViewController = require("./AlmanacWidgetController");
 export = AlmanacWidgetDirective;
 
 class AlmanacWidgetDirective implements ng.IDirective {
     public static htmlName = 'almanacWidget';
 
     public restrict = 'E';
-    public templateUrl = 'app/directives/Almanac/AlmanacWidgetTemplate.html';
+    public template = '<div ng-include="ctrl.templateUrl"></div>';
     public controller = AlmanacViewController;
     public controllerAs = 'ctrl';
     public bindToController = true;
-    public transclude = true;
-    public scope = {};
+    public scope = {
+        "type": "@"
+    };
+
+    public link = {
+        post: ($scope, $element:JQuery, $attrs, $ctrl)=> {
+            var size = $ctrl.getTemplateSize($attrs.type);
+            $element.attr('widget-size', size);
+            $element.attr('data-ss-colspan', AlmanacWidgetDirective.getColumnsFromSize(size));
+        }
+    };
 
     public static create() {
         return new AlmanacWidgetDirective();
+    }
+
+    public static getColumnsFromSize(size) {
+        switch (size) {
+            case 'small':
+                return 1;
+            case 'medium':
+                return 2;
+            case 'large':
+                return 3;
+            default:
+                return 1;
+        }
     }
 
 }
