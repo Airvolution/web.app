880befa3bfe9bd00e98972798f6bc0c4b4820f54
Jared Moore <JMoore@dealersocket.com>
2016-03-01 17:34:37 -0700

    start of search service

---
+++ app/services/SearchService.ts
@@ -0,0 +1,9 @@
+/// <reference path="../../typings/tsd.d.ts" />
+
+export = SearchService;
+
+class SearchService {
+    public static serviceName = "SearchService";
+    static $inject = [];
+    constructor(){}
+}
