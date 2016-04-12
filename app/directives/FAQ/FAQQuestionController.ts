ad7ed8735371afe8c42571a73c20b0e9274d7b05
JDoge <u0729256@utah.edu>
2016-04-11 22:41:13 -0600

    Improved the FAQ page's general style. Style can use additional feedback on design.

--- app/directives/FAQ/FAQQuestionController.ts
+++ app/directives/FAQ/FAQQuestionController.ts
@@ -4,8 +4,19 @@ export = FAQQuestionController;
 
 class FAQQuestionController {
     public question;
+    public chev = 'DOWN';
 
     public static $inject = [];
     constructor() {
+    };
+
+    public toggleChev = function () {
+        var image = angular.element('#img_' + this.question.id);
+        if(this.chev == 'DOWN') {
+            this.chev = 'UP';
+        }
+        else {
+            this.chev = 'DOWN';
+        }
     };
 };
