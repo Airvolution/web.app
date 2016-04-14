63d35dee9e615f62be49b6431fbf82768b2f57c7
JDoge <u0729256@utah.edu>
2016-04-14 17:04:55 -0600

    Made requested changed for pull-request

--- app/directives/About/AboutController.ts
+++ app/directives/About/AboutController.ts
@@ -20,26 +20,25 @@ class AboutController {
 
         var jaredP  = new UserProfile('Jared Potter', 'Backend ASP.NET API, Frontend Angular.JS, and 3rd party data gathering.', 'https://www.linkedin.com/in/jared-potter-34138634', 'https://github.com/JDoge', 'app/assets/images/profilePictures/jared_potter.png');
         var jaredM  = new UserProfile('Jared Moore', 'Frontend Angular.JS', 'https://www.linkedin.com/in/jared-moore-820b2975', 'GitHub: https://github.com/Jmoore1127', 'app/assets/images/profilePictures/jared_moore.png');
-        var taylorW = new UserProfile('Taylor Wilson', 'Frontend Angular.JS', 'https://www.linkedin.com/in/t3ilson', 'GitHub: https://github.com/TJHooker33', 'app/assets/images/profilePictures/taylor_wilson.png');
+        var taylorW = new UserProfile('Taylor Wilson', 'Frontend Angular.JS', 'https://www.linkedin.com/in/t3ilson', 'GitHub: https://github.com/rolyatwilson', 'app/assets/images/profilePictures/taylor_wilson.png');
         var zachL   = new UserProfile('Zach Lobato', 'Backend ASP.NET API, MySQL Database, and 3rd party data gathering.', 'https://www.linkedin.com/in/zachlobato', 'https://github.com/ZachLobato', 'app/assets/images/profilePictures/zach_lobato.png');
 
         this.profiles = [jaredP, jaredM, taylorW, zachL];
     };
 
     public sendEmail() {
-        if(!this.formData.$valid) {
+        if(this.formData.$valid) {
             this.emailAlert = {type: "alert alert-success", message: "Email sent. Thank you."};
             this.formData.$submitted = true;
-            return;
-        }
 
-        this.APIService.PostContactUsEmail(this.formData).then(
-            function (response) {
-                console.log("got valid response"+response);
-            },
-            function (response) {
-                console.error(response);
-            }
-        );
+            this.APIService.PostContactUsEmail(this.formData).then(
+                function (response) {
+                    console.log("got valid response"+response);
+                },
+                function (response) {
+                    console.error(response);
+                }
+            );
+        }
     };
 }
