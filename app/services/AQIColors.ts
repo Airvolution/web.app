291d4ce0a3e768c31c7a115cf3a33572614636c4
Taylor Wilson <rolyat.wilson@gmail.com>
2016-04-15 16:50:54 -0600

    adds parameters to service

--- app/services/AQIColors.ts
+++ app/services/AQIColors.ts
@@ -6,6 +6,7 @@ class AQIColors {
     public static serviceName = 'AQIColors';
 
     private colors;
+    private parameters;
 
     constructor() {
         this.colors = {
@@ -16,9 +17,26 @@ class AQIColors {
             5: '#99004c',
             6: '#7e0023'
         };
+
+        this.parameters = [
+            { name: 'PM1.0',       unit: 'UG/M3'  },
+            { name: 'PM2.5',       unit: 'UG/M3'  },
+            { name: 'PM10',        unit: 'UG/M3'  },
+            { name: 'CO',          unit: 'PPM'    },
+            { name: 'NO2',         unit: 'PPB'    },
+            { name: 'OZONE',       unit: 'PPB'    },
+            { name: 'Altitude',    unit: 'M'      },
+            { name: 'Humidity',    unit: '%'      },
+            { name: 'Temperature', unit: 'C'      },
+            { name: 'Pressure',    unit: 'Pascal' }
+        ];
     }
 
     public getColorFromCategory(number) {
         return this.colors[number];
+    }
+
+    public getParameterList() {
+        return this.parameters;
     }
 }
