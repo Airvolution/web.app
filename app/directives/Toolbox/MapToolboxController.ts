3191882f5d44fdcc7a27e41c8bcdaf65a625cd90
TJHooker33 <rolyat.wilson@gmail.com>
2016-04-16 17:42:01 -0600

    cleaning up functionality

--- app/directives/Toolbox/MapToolboxController.ts
+++ app/directives/Toolbox/MapToolboxController.ts
@@ -8,15 +8,15 @@ class MapToolboxController {
     public searchOptions;
 
     public expanded;
-    public showDetails;
-    public clusters;
-    public currentStation;
-    public stationGroup;
-    public stationGroupMap;
-    public sortOrder;
-    public pollutantOptions;
-    public pollutantOptionsMap;
-    public weatherOptions;
+    //public showDetails;
+    //public clusters;
+    //public currentStation;
+    //public stationGroup;
+    //public stationGroupMap;
+    //public sortOrder;
+    //public pollutantOptions;
+    //public pollutantOptionsMap;
+    //public weatherOptions;
 
     // new variables ^^^ some of previous variables are buggy
     public markerSelection;
@@ -57,18 +57,18 @@ class MapToolboxController {
         this.stationQueryResults = [];
 
         let mtc = this;
-        mtc.clusters = [];
-        mtc.stationGroup = [];
-        mtc.convertMapLayersToArray(mapFactory.createMapLayers().overlays);
-        mtc.getStationGroupFromSelectionService();
-        mtc.setSortOrderForStations('name');
-        mtc.initPollutantOptions();
+        //mtc.clusters = [];
+        //mtc.stationGroup = [];
+        //mtc.convertMapLayersToArray(mapFactory.createMapLayers().overlays);
+        //mtc.getStationGroupFromSelectionService();
+        //mtc.setSortOrderForStations('name');
+        //mtc.initPollutantOptions();
 
-        var deregister = $scope.$parent.$watch('selectedStation', function (val) {
-            mtc.currentStation = val;
-        });
-
-        $scope.$on("$destroy",()=>{deregister();});
+        //var deregister = $scope.$parent.$watch('selectedStation', function (val) {
+        //    mtc.currentStation = val;
+        //});
+        //
+        //$scope.$on("$destroy",()=>{deregister();});
 
     }
 
@@ -86,23 +86,23 @@ class MapToolboxController {
         });
     }
 
-    private getStationGroupFromSelectionService() {
-        this.currentStation = this.selectionService.getCurrentStation();
-        this.stationGroup = this.selectionService.getCurrentStationSelection();
-        this.stationGroupMap = this.selectionService.getCurrentStationSelectionMap();
-    }
+    //private getStationGroupFromSelectionService() {
+    //    this.currentStation = this.selectionService.getCurrentStation();
+    //    this.stationGroup = this.selectionService.getCurrentStationSelection();
+    //    this.stationGroupMap = this.selectionService.getCurrentStationSelectionMap();
+    //}
 
-    public removeMarkerFromGroup(marker) {
-        this.selectionService.removeStationFromSelection(marker);
-        this.selectionService.setCurrentStation(marker);
-        this.$scope.setSelectedStation(marker);
-    }
+    //public removeMarkerFromGroup(marker) {
+    //    this.selectionService.removeStationFromSelection(marker);
+    //    this.selectionService.setCurrentStation(marker);
+    //    this.$scope.setSelectedStation(marker);
+    //}
 
-    public addMarkerToGroup(marker) {
-        this.selectionService.addStationToSelection(marker);
-        this.selectionService.setCurrentStation(marker);
-        this.$scope.setSelectedStation(marker);
-    }
+    //public addMarkerToGroup(marker) {
+    //    this.selectionService.addStationToSelection(marker);
+    //    this.selectionService.setCurrentStation(marker);
+    //    this.$scope.setSelectedStation(marker);
+    //}
 
     public isMarkerInGroup(marker) {
         return this.markerSelectionIds[marker.id] != undefined;
@@ -214,61 +214,34 @@ class MapToolboxController {
         this.$log.log('MapToolboxController: saveSelectionGroup() called. TODO: Implement this functionality.');
     }
 
-    private convertMapLayersToArray(layers) {
-        let self = this;
-        angular.forEach(layers, function(value, key) {
-            self.clusters.push({
-                id: key,
-                name: value.name,
-                visible: value.visible
-            });
-        });
-    }
+    //////////////////////////////////////////////////////
+    // OLD CODE SHOWS HOW TO TOGGLE ON AND OFF CLUSTERS //
+    //private convertMapLayersToArray(layers) {
+    //    let self = this;
+    //    angular.forEach(layers, function(value, key) {
+    //        self.clusters.push({
+    //            id: key,
+    //            name: value.name,
+    //            visible: value.visible
+    //        });
+    //    });
+    //}
+    //
+    //public showAllClusters() {
+    //    this.$scope.showAllClusters();
+    //    angular.forEach(this.clusters, function (cluster) {
+    //        cluster['visible'] = true;
+    //    });
+    //}
+    //
+    //public hideAllClusters() {
+    //    this.$scope.hideAllClusters();
+    //    angular.forEach(this.clusters, function (cluster) {
+    //        cluster['visible'] = false;
+    //    });
+    //}
+    //////////////////////////////////////////////////////
 
-
-    public getMarkerNames() {
-        return this.mapFactory.getMarkerNames();
-    }
-
-
-    public showAllClusters() {
-        this.$scope.showAllClusters();
-        angular.forEach(this.clusters, function (cluster) {
-            cluster['visible'] = true;
-        });
-    }
-
-    public hideAllClusters() {
-        this.$scope.hideAllClusters();
-        angular.forEach(this.clusters, function (cluster) {
-            cluster['visible'] = false;
-        });
-    }
-
-    public setSortOrderForStations(order) {
-        switch (order) {
-            case 'name':
-                this.sortOrder = '-name';
-                break;
-            case 'agency':
-                this.sortOrder = '-agency';
-                break;
-            case 'id':
-                this.sortOrder = '-id';
-                break;
-            case 'city':
-                this.sortOrder = '-city';
-                break;
-            case 'state':
-                this.sortOrder = '-state';
-                break;
-            case 'postal':
-                this.sortOrder = '-postal';
-                break;
-            default:
-                this.sortOrder = '-name';
-        };
-    }
 
     public setSelectedStation(marker) {
         this.selectionService.setCurrentStation(marker);
@@ -277,67 +250,103 @@ class MapToolboxController {
         this.$scope.centerOnMarker(marker.location);
     }
 
-    public togglePollutantOption(pollutant) {
-        pollutant.selected = !pollutant.selected;
-        this.selectionService.updatePollutantSelectionWith(pollutant.kind);
-    }
 
-    private initPollutantOptions() {
-        let self = this;
-        self.pollutantOptionsMap = self.getPollutantOptionsMap();
 
-        let selection = self.selectionService.getCurrentPollutantSelection();
-        let options = self.getPollutantOptions();
-        angular.forEach(selection, function(value) {
-            let key = self.pollutantOptionsMap[value];
-            options[key].selected = true;
-        });
-        self.pollutantOptions = options;
-    }
+    //public getMarkerNames() {
+    //    return this.mapFactory.getMarkerNames();
+    //}
 
-    private getPollutantOptions() {
-        return [
-            {
-                kind: 'PM2.5',
-                name: 'Particulate Matter 2.5',
-                selected: false
-            },
-            {
-                kind: 'PM10',
-                name: 'Particulate Matter 10',
-                selected: false
-            },
-            {
-                kind: 'CO',
-                name: 'Carbon Monoxide',
-                selected: false
-            },
-            {
-                kind: 'CO2',
-                name: 'Carbon Dioxide',
-                selected: false
-            },
-            {
-                kind: 'NO2',
-                name: 'Nitrogen Dioxide',
-                selected: false
-            },
-            {
-                kind: 'OZONE',
-                name: 'Ozone',
-                selected: false
-            },
-        ];
-    }
 
-    private getPollutantOptionsMap() {
-        return {
-            'PM2.5': 0,
-            'PM10': 1,
-            'CO': 2,
-            'CO2': 3,
-            'NO2': 4,
-            'OZONE': 5
-        };
-    }
+
+
+    //public setSortOrderForStations(order) {
+    //    switch (order) {
+    //        case 'name':
+    //            this.sortOrder = '-name';
+    //            break;
+    //        case 'agency':
+    //            this.sortOrder = '-agency';
+    //            break;
+    //        case 'id':
+    //            this.sortOrder = '-id';
+    //            break;
+    //        case 'city':
+    //            this.sortOrder = '-city';
+    //            break;
+    //        case 'state':
+    //            this.sortOrder = '-state';
+    //            break;
+    //        case 'postal':
+    //            this.sortOrder = '-postal';
+    //            break;
+    //        default:
+    //            this.sortOrder = '-name';
+    //    };
+    //}
+
+
+
+    //public togglePollutantOption(pollutant) {
+    //    pollutant.selected = !pollutant.selected;
+    //    this.selectionService.updatePollutantSelectionWith(pollutant.kind);
+    //}
+
+    //private initPollutantOptions() {
+    //    let self = this;
+    //    self.pollutantOptionsMap = self.getPollutantOptionsMap();
+    //
+    //    let selection = self.selectionService.getCurrentPollutantSelection();
+    //    let options = self.getPollutantOptions();
+    //    angular.forEach(selection, function(value) {
+    //        let key = self.pollutantOptionsMap[value];
+    //        options[key].selected = true;
+    //    });
+    //    self.pollutantOptions = options;
+    //}
+
+    //private getPollutantOptions() {
+    //    return [
+    //        {
+    //            kind: 'PM2.5',
+    //            name: 'Particulate Matter 2.5',
+    //            selected: false
+    //        },
+    //        {
+    //            kind: 'PM10',
+    //            name: 'Particulate Matter 10',
+    //            selected: false
+    //        },
+    //        {
+    //            kind: 'CO',
+    //            name: 'Carbon Monoxide',
+    //            selected: false
+    //        },
+    //        {
+    //            kind: 'CO2',
+    //            name: 'Carbon Dioxide',
+    //            selected: false
+    //        },
+    //        {
+    //            kind: 'NO2',
+    //            name: 'Nitrogen Dioxide',
+    //            selected: false
+    //        },
+    //        {
+    //            kind: 'OZONE',
+    //            name: 'Ozone',
+    //            selected: false
+    //        },
+    //    ];
+    //}
+
+    //private getPollutantOptionsMap() {
+    //    return {
+    //        'PM2.5': 0,
+    //        'PM10': 1,
+    //        'CO': 2,
+    //        'CO2': 3,
+    //        'NO2': 4,
+    //        'OZONE': 5
+    //    };
+    //}
 }
