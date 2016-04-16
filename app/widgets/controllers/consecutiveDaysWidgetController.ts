aa7d62d6bb4cf4d563b4e8c90cb9ab1c464e7e60
JDoge <u0729256@utah.edu>
2016-04-16 16:32:38 -0600

    Removed un-needed ng-class conditionals. Added emoji property to controller.

--- app/widgets/controllers/consecutiveDaysWidgetController.ts
+++ app/widgets/controllers/consecutiveDaysWidgetController.ts
@@ -6,10 +6,12 @@ class ConsecutiveDaysWidgetController {
     public static name = 'ConsecutiveDaysWidgetController';
     public static $inject = ['ConsecutiveDaysFactory'];
     public consecutiveDays;
+    public faEmoji;
     constructor(ConsecutiveDaysFactory) {
         var self = this;
         ConsecutiveDaysFactory.getConsecutiveDays().then((data)=>{
             self.consecutiveDays = data;
+            self.getEmoji();
         });
     }
 
@@ -23,6 +25,20 @@ class ConsecutiveDaysWidgetController {
         if (input != null){
             input = input.toLowerCase();
             return input.substring(0,1).toUpperCase()+input.substring(1);
+        }
+    }
+
+    public getEmoji(){
+        var category = this.consecutiveDays.category;
+
+        if(category == 'green') {
+            this.faEmoji = 'fa-smile-o';
+        }
+        else if(category == 'yellow') {
+            this.faEmoji = 'fa-meh-o';
+        }
+        else {
+            this.faEmoji = 'fa-frown-o';
         }
     }
 }
