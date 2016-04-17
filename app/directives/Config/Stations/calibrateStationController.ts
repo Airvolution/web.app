a6c7c59a5c8e71ff4d34570f541d1d2bc8a9e7c1
TJHooker33 <rolyat.wilson@gmail.com>
2016-04-17 02:24:14 -0600

    adds dropdowns for new parameter adjustments

--- app/directives/Config/Stations/calibrateStationController.ts
+++ app/directives/Config/Stations/calibrateStationController.ts
@@ -6,22 +6,34 @@ class CalibrateStationController {
     public station;
     public existingAdjustments;
     public newAdjustments;
-    public static $inject = ['$state','$stateParams','APIService','$scope'];
+    public unadjustedParameters;
+    public loading;
+    public static $inject = ['$scope', '$state','$stateParams','APIService', 'AQIColors'];
     public constructor(
+        private $scope,
         private $state,
         private $stateParams,
         private APIService,
-        private $scope
+        private AQIColors
+
     ){
+        this.loading = true;
+        this.newAdjustments = [];
+        this.unadjustedParameters = angular.copy(this.AQIColors.getParameterList());
+
         var self = this;
         this.APIService.getStation($stateParams.id).then((station) => {
             self.station = station;
             self.APIService.getStationAdjustments(station.id).then((adjustments) => {
                 self.existingAdjustments = adjustments;
+                self.loading = false;
+            }, (error) => {
+                self.loading = false;
+                // TODO: handle error nicely
             });
         });
 
-        this.newAdjustments = [];
+
 
         $scope.configureModal('Calibrate Station '+$stateParams.id,
             "Save",
@@ -36,5 +48,27 @@ class CalibrateStationController {
 
         //TODO Load Adjustment params here
         console.log("Calibrating station "+$stateParams.id);
+    }
+
+    public deleteFromExistingAdjustments(adjustment) {
+        let index = this.existingAdjustments.indexOf(adjustment);
+        this.existingAdjustments.splice(index, 1);
+    }
+
+    public deleteFromNewAdjustments(adjustment) {
+        let index = this.newAdjustments.indexOf(adjustment);
+        this.newAdjustments.splice(index, 1);
+    }
+
+    public addNewAdjustment() {
+        this.newAdjustments.push({
+            parameter: this.unadjustedParameters[0],
+            scaleFactor: 1.0,
+            shiftFactor: 0.0
+        });
+    }
+
+    public setAdjustmentParameter(adjustment, parameter) {
+        adjustment.parameter = parameter;
     }
 }
--- app/services/AQIColors.ts
+++ app/services/AQIColors.ts
@@ -6,19 +6,37 @@ class AQIColors {
     public static serviceName = 'AQIColors';
 
     private colors;
+    private parameters;
 
     constructor() {
         this.colors = {
-            1: '#00e400',
-            2: '#ffff00',
-            3: '#ff7e00',
-            4: '#ff0000',
-            5: '#99004c',
-            6: '#7e0023'
+            1: '#00e400', // green  for category 1
+            2: '#ffff00', // yellow for category 2
+            3: '#ff7e00', // orange for category 3
+            4: '#ff0000', // red    for category 4
+            5: '#99004c', // purple for category 5
+            6: '#7e0023'  // maroon for category 6
         };
+
+        this.parameters = [
+                { name: 'PM1.0',       unit: 'UG/M3'  },
+                { name: 'PM2.5',       unit: 'UG/M3'  },
+                { name: 'PM10',        unit: 'UG/M3'  },
+                { name: 'CO',          unit: 'PPM'    },
+                { name: 'NO2',         unit: 'PPB'    },
+                { name: 'OZONE',       unit: 'PPB'    },
+                { name: 'Altitude',    unit: 'M'      },
+                { name: 'Humidity',    unit: '%'      },
+                { name: 'Temperature', unit: 'C'      },
+                { name: 'Pressure',    unit: 'Pascal' }
+        ];
     }
 
     public getColorFromCategory(number) {
         return this.colors[number];
+    }
+
+    public getParameterList() {
+        return this.parameters;
     }
 }
