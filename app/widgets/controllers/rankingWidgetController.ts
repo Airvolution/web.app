7e7d93cdb15b039872184ab4b8135a25a5bc08df
Zach Lobato <zacharyisaiahlobato@hotmail.com>
2016-04-17 16:14:45 -0600

    basic functionality

--- app/widgets/controllers/rankingWidgetController.ts
+++ app/widgets/controllers/rankingWidgetController.ts
@@ -8,6 +8,12 @@ class RankingWidgetController {
     public someValue;
     public loading;
 
+    public bestStation;
+    public worstStation;
+    public yourStation;
+
+    public nationalRank;
+
     public static $inject = ['$scope', '$log', 'APIService'];
     public constructor (
         private $scope,
@@ -15,20 +21,44 @@ class RankingWidgetController {
         private APIService
     ) {
         let self = this;
-        let bounds = {'northEast': {'lat': 89, 'lng': 179}, 'southWest': {'lat': -89, 'lng': -179}};
-        this.APIService.asyncGetMarkersInside(bounds).then((response) => {
-            self.someFunction(response.data);
+
+        let unregisterDailies = $scope.$watch('dailies', (dailies) => {
+            let bounds = {'northEast': {'lat': 89, 'lng': 179}, 'southWest': {'lat': -89, 'lng': -179}};
+            this.APIService.asyncGetMarkersInside(bounds).then((response) => {
+                self.someFunction(dailies, response.data);
+
+                this.loading = false;
+            });
+        });
+
+        $scope.$on("$destroy", () => {
+            unregisterDailies();
         });
     }
 
-    public someFunction(markers) {
-        if (!markers) {
+    public getStationAndRanking(stationID, stations) {
+        for (var i = 0; i < stations.length; i++){
+            if (stations[i].id==stationID){
+                this.nationalRank = i;
+                return stations[i];
+            }
+
+        }
+    }
+
+    public someFunction(dailies, markers) {
+        if (!markers || !dailies) {
             this.loading = true;
             return;
         }
 
-        this.someValue = markers[0].aqi;
+        markers.sort((a, b) => a.aqi - b.aqi);
 
-        this.loading = false;
+        this.yourStation = this.getStationAndRanking(dailies[0].station_Id, markers);
+        this.bestStation = markers[0];
+        this.worstStation = markers[markers.length-1];
+
+
+
     }
 }
